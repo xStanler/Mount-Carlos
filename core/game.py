7baba42207d3cad74155a3e7b9e88d7ab4e8adb5
Stanisław Chmielewski <kontakt.stanislaw.chmielewski@gmail.com>
2026-06-01 14:12:15 +0200

    ui-menu: Added pause menu and icon for the game

--- core/game.py
+++ core/game.py
@@ -1,9 +1,10 @@
 from engine.map import Map, Tiles
 from engine.player import Player
 from engine.camera import Camera
-from ui.menus import MainMenu
+from ui.menus import MainMenu, PauseMenu
 import pygame
 from pygame.locals import *
+from pathlib import Path
 from enum import Enum
 
 class GameState(Enum):
@@ -19,9 +20,14 @@ class Game:
 
         self.screen = pygame.display.set_mode((640, 640), pygame.RESIZABLE | pygame.SCALED, vsync=1)
         self.clock = pygame.time.Clock()
+
+        icon_path = Path(__file__).parent.parent / "assets/icon.png"
+        self.icon = pygame.image.load(icon_path).convert_alpha()
+        pygame.display.set_icon(self.icon)
         
         self.running = True
         self.main_menu = MainMenu(*self.screen.get_size())
+        self.pause_menu = PauseMenu(*self.screen.get_size())
         self.game_state = GameState.MENU
 
         self.camera = Camera()
@@ -34,15 +40,15 @@ class Game:
         self.player = Player(sx, sy)
     
     def run(self):
-        self.mouse_clicked = False
-
         while self.running:
+            self.mouse_clicked = False
             self.mouse_pos = pygame.mouse.get_pos()
             self.handle_events()
             self.handle_states()
             # self.update()
             # self.render()
 
+            pygame.display.flip()
             self.clock.tick(60)
 
         pygame.quit()
@@ -58,8 +64,10 @@ class Game:
                 pygame.RESIZABLE | pygame.SCALED
             )
 
-            if event.type == pygame.MOUSEBUTTONUP and event.button == 1:
+            if event.type == pygame.MOUSEBUTTONDOWN and event.button == 1:
                 self.mouse_clicked = True
+            if event.type == pygame.KEYDOWN and event.key == pygame.K_SPACE:
+                self.game_state = GameState.PAUSE
 
     def handle_states(self):
         if self.game_state == GameState.MENU:
@@ -71,7 +79,15 @@ class Game:
                 self.running = False
 
             self.main_menu.render(self.screen)
-            pygame.display.flip()
+        elif self.game_state == GameState.PAUSE:
+            pause_action = self.pause_menu.update(self.mouse_pos, self.mouse_clicked)
+
+            if pause_action == "CONTINUE":
+                self.game_state = GameState.GAME
+            elif pause_action == "QUIT":
+                self.running = False
+
+            self.pause_menu.render(self.screen)
         elif self.game_state == GameState.GAME:
             self.update()
             self.render()
@@ -85,4 +101,4 @@ class Game:
         self.screen.fill((192, 203, 220))
         self.map.render(self.screen, self.camera)
         self.player.render(self.screen, self.camera)
-        pygame.display.flip()
+        # pygame.display.flip()
--- ui/menus.py
+++ ui/menus.py
@@ -4,6 +4,8 @@ from pathlib import Path
 class Button:
     def __init__(self, text, x, y, width, height, base_color, hover_color, font):
         self.text = text
+        self.width = width
+        self.height = height
         self.rect = pygame.Rect(x, y, width, height)
         self.base_color = base_color
         self.hover_color = hover_color
@@ -15,6 +17,8 @@ class Button:
             self.current_color = self.hover_color
         else:
             self.current_color = self.base_color
+    def set_position(self, x, y):
+        self.rect = pygame.Rect(x, y, self.width, self.height)
 
     def draw(self, screen):
         pygame.draw.rect(screen, self.current_color, self.rect, border_radius=10)
@@ -35,13 +39,13 @@ class MainMenu():
         self.button_font = pygame.font.Font(str(font_path), 32)
 
         btn_w, btn_h = 240, 50
-        btn_x = (screen_width // 2) - (btn_w // 2)
+        btn_x = (self.screen_width // 2) - (btn_w // 2)
 
         self.start_button = Button("ROZPOCZNIJ GRĘ", btn_x, 250, btn_w, btn_h, (60, 83, 60), (51, 71, 51), self.button_font)
         self.quit_button = Button("WYJDŹ", btn_x, 330, btn_w, btn_h, (60, 82, 60), (51, 71, 51), self.button_font)
 
         self.title_surf = self.title_font.render("MOUNT CARLOS", True, (193, 140, 93))
-        self.title_rect = self.title_surf.get_rect(center=(screen_width // 2, 120))
+        self.title_rect = self.title_surf.get_rect(center=(self.screen_width // 2, 120))
 
     def update(self, mouse_pos, mouse_clicked):
         self.start_button.update(mouse_pos)
@@ -55,9 +59,54 @@ class MainMenu():
         return "NONE"
 
     def render(self, screen):
+        self.screen_width, self.screen_height = screen.get_size()
+        self.start_button.set_position((self.screen_width // 2) - (240 // 2), 250)
+        self.quit_button.set_position((self.screen_width // 2) - (240 // 2), 330)
+        self.title_rect = self.title_surf.get_rect(center=(self.screen_width // 2, 120))
         # screen.fill((73, 88, 103))
         screen.fill((102, 122, 143))
 
         screen.blit(self.title_surf, self.title_rect)
         self.start_button.draw(screen)
+        self.quit_button.draw(screen)
+
+class PauseMenu():
+    def __init__(self, screen_width, screen_height):
+        self.screen_width = screen_width
+        self.screen_height = screen_height
+
+        font_path = Path(__file__).parent.parent / "assets/VT323-Regular.ttf"
+        self.title_font = pygame.font.Font(str(font_path), 72)
+        self.button_font = pygame.font.Font(str(font_path), 32)
+
+        btn_w, btn_h = 240, 50
+        btn_x = (self.screen_width // 2) - (btn_w // 2)
+
+        self.continue_button = Button("KONTYNUUJ GRĘ", btn_x, 250, btn_w, btn_h, (60, 83, 60), (51, 71, 51), self.button_font)
+        self.quit_button = Button("WYJDŹ", btn_x, 330, btn_w, btn_h, (60, 82, 60), (51, 71, 51), self.button_font)
+
+        self.title_surf = self.title_font.render("GRA PRZERWANA", True, (193, 140, 93))
+        self.title_rect = self.title_surf.get_rect(center=(self.screen_width // 2, 120))
+
+    def update(self, mouse_pos, mouse_clicked):
+        self.continue_button.update(mouse_pos)
+        self.quit_button.update(mouse_pos)
+
+        if self.continue_button.is_clicked(mouse_pos, mouse_clicked):
+            return "CONTINUE"
+        if self.quit_button.is_clicked(mouse_pos, mouse_clicked):
+            return "QUIT"
+
+        return "NONE"
+
+    def render(self, screen):
+        self.screen_width, self.screen_height = screen.get_size()
+        self.continue_button.set_position((self.screen_width // 2) - (240 // 2), 250)
+        self.quit_button.set_position((self.screen_width // 2) - (240 // 2), 330)
+        self.title_rect = self.title_surf.get_rect(center=(self.screen_width // 2, 120))
+        # screen.fill((73, 88, 103))
+        screen.fill((102, 122, 143))
+
+        screen.blit(self.title_surf, self.title_rect)
+        self.continue_button.draw(screen)
         self.quit_button.draw(screen)
